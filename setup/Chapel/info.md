b27b41f4f106e6b9a0417a0948be037c0e3f68f3
Kevin Song <chipbuster@gmail.com>
2022-03-19 15:34:01 -0700

    Add installation for Chapel

---
+++ setup/Chapel/info.md
@@ -0,0 +1,13 @@
+Ada
+===
+
+Version: chpl 1.15.0
+Path: /usr/local/src/chapel-1.15.0/bin/linux64/chpl
+
+Provided by: Need to download + build
+
+Project Link: https://chapel-lang.org/docs/1.15/usingchapel/QUICKSTART.html
+Download Link: https://github.com/chapel-lang/chapel/releases/download/1.15.0/chapel-1.15.0.tar.gz
+Prerequisites: build-essential
+Installation Notes: No install step--copy folder to final location
+Other: N/A
---
+++ setup/Chapel/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/../vars.sh"
+
+dl_link="https://github.com/chapel-lang/chapel/releases/download/1.15.0/chapel-1.15.0.tar.gz"
+tarname="chapel-1.15.0.tar.gz"
+dirname="chapel-1.15.0"
+
+## Installation steps
+
+# Download and enter the directory
+cd "$TMPDIR"
+wget "$dl_link"
+tar -xf "$tarname"
+
+rm "$tarname"
+
+pushd "$dirname"
+
+# Follow instructions from webpage
+source util/setchplenv.bash
+make -j "$NCPUS"
+
+# Move into final location
+popd
+final_location="/usr/local/src/$dirname"
+sudo rm -rf "$final_location"
+sudo mv "$dirname" "$final_location"
