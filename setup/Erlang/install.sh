c232cd340d2e1616391225ef4443cc222158c687
Kevin Song <chipbuster@gmail.com>
2022-03-19 16:08:31 -0700

    Add Erlang

---
+++ setup/Erlang/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/../vars.sh"
+
+## Installation steps
+
+sudo apt-get install erlang
