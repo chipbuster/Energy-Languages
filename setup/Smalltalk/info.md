a971cc02b330a56f10c311e542030ab502d9ab0d
Kevin Song <chipbuster@gmail.com>
2022-03-20 14:40:13 -0500

    add (skip) smalltalk

---
+++ setup/Smalltalk/info.md
@@ -0,0 +1,10 @@
+Smalltalk
+===
+
+Version: Not in original tests
+
+Project Link: N/A
+Download Link: N/A
+Prerequisites: N/A
+Installation Notes: N/A
+Other: N/A
---
+++ setup/Smalltalk/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/../vars.sh"
+
+## Installation steps
+
+echo "Smalltalk was not installed for the 2017 work, so we are skipping it"
